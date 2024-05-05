06515903c242c7d60b11120321bfeb5defe6f757
shivam-appboxer <shivam@appboxer.com.au>
2024-05-06 01:22:07 +0530

    fixed

--- src/app/dashboard/members/page.tsx
+++ src/app/dashboard/members/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import dynamic from "next/dynamic";
 import React from "react";
-import MemberList from "./MemberList";
-
+const MemberList = dynamic(() => import("./MemberList"), {
+  ssr: false,
+});
 const page = () => {
   return <MemberList />;
 };
