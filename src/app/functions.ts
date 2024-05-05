de09492db599f037867de12b439547043336cc85
shivam-appboxer <shivam@appboxer.com.au>
2024-05-06 04:37:52 +0530

    fixed

--- src/app/functions.ts
+++ src/app/functions.ts
@@ -29,24 +29,34 @@ export function getDifferenceInDays(endDate) {
   return daysDifference;
 }
 
+// export function daysUntilExpiration(date) {
+//   const currentDate = new Date();
+//   currentDate.setHours(0, 0, 0, 0);
+
+//   const expirationDate = new Date(date);
+//   expirationDate.setHours(0, 0, 0, 0);
+
+//   // Calculate the difference in milliseconds
+//   const difference = expirationDate.getTime() - currentDate.getTime();
+
+//   // Convert milliseconds to days
+//   const daysDifference = Math.ceil(difference / (1000 * 60 * 60 * 24));
+
+//   if (daysDifference > 0) {
+//     return daysDifference; // Return days left until expiration
+//   } else {
+//     return daysDifference; // Return days passed since expiration
+//   }
+// }
+
 export function daysUntilExpiration(date) {
-  const currentDate = new Date();
-  currentDate.setHours(0, 0, 0, 0);
+  const currentDate = moment().tz("Asia/Kolkata").startOf("day");
+  const expirationDate = moment(date).tz("Asia/Kolkata").startOf("day");
 
-  const expirationDate = new Date(date);
-  expirationDate.setHours(0, 0, 0, 0);
+  // Calculate the difference in days
+  const daysDifference = expirationDate.diff(currentDate, "days");
 
-  // Calculate the difference in milliseconds
-  const difference = expirationDate.getTime() - currentDate.getTime();
-
-  // Convert milliseconds to days
-  const daysDifference = Math.ceil(difference / (1000 * 60 * 60 * 24));
-
-  if (daysDifference > 0) {
-    return daysDifference; // Return days left until expiration
-  } else {
-    return daysDifference; // Return days passed since expiration
-  }
+  return daysDifference;
 }
 
 export function spaceToCamelCase(str) {
