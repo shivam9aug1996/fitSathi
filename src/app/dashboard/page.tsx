1697e397b1fa215155b28121c93cbcb025f7a884
shivam-appboxer <shivam@appboxer.com.au>
2024-05-08 23:19:02 +0530

    fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,15 @@
-import React, { Suspense } from "react";
-import CreateGym from "../components/CreateGym";
-import Header from "../components/Header";
-import GymDashboard from "./GymDashboard";
-import GymList from "./GymList";
-
+import dynamic from "next/dynamic";
+import React from "react";
+// import GymDashboard from "./GymDashboard";
+const GymDashboard = dynamic(() => import("./GymDashboard"), {
+  ssr: false,
+});
 const page = () => {
   return (
     <>
       {/* <GymList /> */}
-      <Suspense>
-        <GymDashboard />
-      </Suspense>
+
+      <GymDashboard />
     </>
   );
 };
