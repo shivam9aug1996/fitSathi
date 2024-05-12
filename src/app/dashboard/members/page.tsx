91e1bb8634b28ab6a5c5dcef628dd56d4e5995cb
shivam-appboxer <shivam@appboxer.com.au>
2024-05-12 14:49:12 +0530

    fixed

--- src/app/dashboard/members/page.tsx
+++ src/app/dashboard/members/page.tsx
@@ -3,11 +3,11 @@ import Loader from "@/app/components/Loader";
 import TableLoader from "@/app/components/TableLoader";
 import dynamic from "next/dynamic";
 import React from "react";
-// import MemberList from "./MemberList";
-const MemberList = dynamic(() => import("./MemberList"), {
-  ssr: false,
-  loading: () => <Loader />,
-});
+import MemberList from "./MemberList";
+// const MemberList = dynamic(() => import("./MemberList"), {
+//   ssr: false,
+//   loading: () => <Loader />,
+// });
 const page = () => {
   return <MemberList />;
 };
--- src/app/dashboard/plans/page.tsx
+++ src/app/dashboard/plans/page.tsx
@@ -1,11 +1,11 @@
 import dynamic from "next/dynamic";
 import React from "react";
 import Loader from "./loading";
-// import PlanList from "./PlanList";
-const PlanList = dynamic(() => import("./PlanList"), {
-  ssr: false,
-  loading: () => <Loader />,
-});
+import PlanList from "./PlanList";
+// const PlanList = dynamic(() => import("./PlanList"), {
+//   ssr: false,
+//   loading: () => <Loader />,
+// });
 const page = () => {
   return <PlanList />;
 };
